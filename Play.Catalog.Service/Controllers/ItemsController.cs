8d1a168deda78c711e6ec695e9436f04d33a0b13
NadejdaChitakova <nadejda.chitakova13@gmail.com>
2022-11-25 16:36:53 +0200

    Add project files.

---
+++ Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Catalog.Service.Controllers
+{
+    public class ItemsController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Play.Catalog.Service/Model/ItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Play.Catalog.Service.Model
+{
+    public class ItemDTO
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+    }
+}
