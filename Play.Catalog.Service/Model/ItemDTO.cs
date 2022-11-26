ab2fee987b6537d105b6a0f96464d8f9805ade59
NadejdaChitakova <nadejda.chitakova13@gmail.com>
2022-11-26 16:45:15 +0200

    Model validation.

--- Play.Catalog.Service/Model/ItemDTO.cs
+++ Play.Catalog.Service/Model/ItemDTO.cs
@@ -1,11 +1,19 @@
-﻿namespace Play.Catalog.Service.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Play.Catalog.Service.Model
 {
     public class ItemDTO
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
         public string? Description { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
         public decimal Price { get; set; }
+
         public DateTime CreatedAt { get; set; }
     }
 }
